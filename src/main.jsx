24fc8ddfa15ecdea93dbba4f9c3dc9d28ae7ab69
Người Làm Tình Màn Ảnh <153808638+LeMinh0706@users.noreply.github.com>
2024-06-23 00:00:47 +0700

    build base

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Product from './Pages/Product.jsx'
+import NotFound from './Pages/NotFound.jsx'
+import ProductDetail from './Pages/ProductDetail.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} >
+          <Route index element={<Product />} />
+          <Route path=':id' element={<ProductDetail />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
