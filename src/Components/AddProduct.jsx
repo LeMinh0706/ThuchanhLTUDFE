6a98e2a454e5f69def635a75fb038e072b7e6156
Người Làm Tình Màn Ảnh <153808638+LeMinh0706@users.noreply.github.com>
2024-06-23 23:24:51 +0700

    base add

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllCategories } from '../Service/CategoryService'
 
 const AddProduct = () => {
     const [name, setName] = useState('')
     const [des, setDes] = useState('')
     const [price, setPrice] = useState('')
     const [img, setImg] = useState([])
+    const [category, setCategory] = useState([])
+    const [cate, setCate] = useState()
+
+    useEffect(() => {
+        getAllCate()
+    }, [])
 
     // console.log(name);
     // console.log(des);
     // console.log(price);
     // console.log(img);
+    const getAllCate = async () => {
+        try {
+            const res = await getAllCategories()
+            console.log(res);
+            setCategory(res.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleCateChange = (e) => {
+        setCate(e.target.value)
+    }
+
     const handleNameChange = (e) => {
         setName(e.target.value)
     }
@@ -24,7 +45,7 @@ const AddProduct = () => {
     }
     const handleSubmit = async () => {
         try {
-            alert(name + '-' + des + '-' + price)
+            alert(name + '-' + des + '-' + price + '-' + cate)
         } catch (error) {
             console.log(error);
         }
@@ -41,6 +62,13 @@ const AddProduct = () => {
             <input className='p-3 border-2 rounded-lg' id='des' type="text" onChange={handleDesChange} value={des} />
             <label className='text-gray-500 font-medium' htmlFor="file">Image:</label>
             <input className='p-3 border-2 rounded-lg' id='file' multiple type="file" onChange={handleFileChange} />
+            <select className='p-3 border' name="" id="category" onChange={handleCateChange} value={cate}>
+                {category.map((item) => (
+                    <>
+                        <option className='text-black rounded-md' value={item.name}>{item.name}</option>
+                    </>
+                ))}
+            </select>
             <button className='p-3 w-[120px] bg-green-400 text-white self-end border rounded-md' onClick={handleSubmit}>
                 Add product
             </button>
